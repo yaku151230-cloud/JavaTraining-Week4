1a8981ce1f916bd37682a98f5f1cf0d0878e54ca
yaku151230-cloud <yaku.151230@gmail.com>
2026-02-08 21:02:53 +0900

    Update HelloController.java

--- src/main/java.com.example.hellospring/HelloController.java
+++ src/main/java.com.example.hellospring/HelloController.java
@@ -3,6 +3,7 @@ package com.example.hellospring;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Helloのコントローラー
 @RestController
 public class HelloController {
 
