efc4fc2deb7abf6a5845fee4a0bd8b797834365b
yaku151230-cloud <yaku.151230@gmail.com>
2026-03-08 14:31:20 +0900

    Create TaskControllerTest.java

---
+++ src/test/java/com/example/taskapp/TaskControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 認証なしアクセスはログインページへリダイレクト() throws Exception {
+        mockMvc.perform(get("/tasks"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    void 認証ありなら一覧ページが表示される() throws Exception {
+        mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tasks/index"));
+    }
+}
