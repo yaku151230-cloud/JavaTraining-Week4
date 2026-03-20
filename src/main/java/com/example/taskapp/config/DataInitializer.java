c1d616b38af533854c8a3a4895873a60ec57ef35
yaku151230-cloud <yaku.151230@gmail.com>
2026-03-20 14:23:36 +0900

    Create DataInitializer.java

---
+++ src/main/java/com/example/taskapp/config/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.example.taskapp.config;
+
+import com.example.taskapp.entity.UserAccount;
+import com.example.taskapp.repository.UserAccountRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// アプリケーション起動時に初期データを投入するクラスを作成
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initUsers(UserAccountRepository repository,
+                                PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            if (repository.findByUsername("user").isEmpty()) {
+                repository.save(
+                        new UserAccount(
+                                "user",
+                                passwordEncoder.encode("password"),
+                                "USER"
+                        )
+                );
+            }
+        };
+    }
+}
