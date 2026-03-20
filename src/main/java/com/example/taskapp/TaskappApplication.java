ab733757f40de13ccc2fe277a75b50b6d0e66f15
yaku151230-cloud <yaku.151230@gmail.com>
2026-03-20 14:32:39 +0900

    Create TaskappApplication.java

---
+++ src/main/java/com/example/taskapp/TaskappApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskappApplication {
+
+    // アプリケーションのメインクラスを作成
+    public static void main(String[] args) {
+        SpringApplication.run(TaskappApplication.class, args);
+    }
+}
